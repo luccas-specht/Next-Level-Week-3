3b8c639e231d1567c8cef1454536cd694f943fe7
luccas-specht <luccasspecht70@gmail.com>
2020-10-14 21:53:12 -0300

    finalizando dia um

--- site/src/ui/pages/orphanages-map/orphanages-map.page.tsx
+++ site/src/ui/pages/orphanages-map/orphanages-map.page.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css'
 import mapMarker from '../../assets/svg/map-marker.svg';
+import './orphanages-map.style.css';
 
 const OrphanagesMap = () => {
     return (
@@ -8,8 +14,38 @@ const OrphanagesMap = () => {
             <aside>
                 <header>
                     <img src={mapMarker} alt="marcador de orfanato" />
+                    <h2>
+                        Escolha um orfanato no mapa
+                    </h2>
+                    <p>
+                        Muitas crianças estão esperando à sua visita :)
+                    </p>
                 </header>
+
+                <footer>
+                    <strong>
+                        Esteio
+                    </strong>
+                    <span>
+                        Rio Grande do Sul
+                    </span>
+                </footer>
             </aside>
+
+            <Map
+                center={[-29.8495465, -51.1397162]}
+                zoom={15}
+                style={{ width: '100%', height: '100%' }}
+            >
+                {/* <TileLayer
+                    url='https://a.tile.openstreetmap.org//{z}/{x}/{y}.png' /> */}
+                <TileLayer
+                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                />
+            </Map>
+            <Link to='/' className='link-to-create-orphanage'>
+                <FiPlus size={32} color='#fff' />
+            </Link >
         </div>
     );
 }
